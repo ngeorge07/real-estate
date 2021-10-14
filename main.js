b9d156bec319a4e9b668274cef53115dc5afc06a
George <geor2335@stud.kea.dk>
2021-10-15 01:31:37 +0200

    grab data from wp

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 function getData() {
-  fetch("https://georgendesign.com/Module8/real-estate/wp-json/wp/v2/house")
+  fetch(
+    "https://georgendesign.com/Module8/real-estate/wp-json/wp/v2/house?order=asc"
+  )
     .then((response) => response.json())
     .then(showPosts);
 }
 
 function showPosts(e) {
-  //   console.log(e);
+  console.log(e);
 
   e.forEach((e) => {
-    // console.log(e.featured_media);
     fetch(
       `https://georgendesign.com/Module8/real-estate/wp-json/wp/v2/media/${e.featured_media}`
     )
@@ -17,9 +18,15 @@ function showPosts(e) {
         const temp = document.querySelector("template").content;
         const clone = temp.cloneNode(true);
 
-        clone.querySelector("img").src = i.source_url;
-
         clone.querySelector("h2").textContent = e.title.rendered;
+
+        clone.querySelector("img").src = i.source_url;
+        clone.querySelector("img").alt = i.alt_text;
+
+        clone.querySelector("p:first-of-type").textContent = `${e.bed} BED`;
+        clone.querySelector("p:nth-of-type(2)").textContent = `${e.bath} BATH`;
+        clone.querySelector("p:last-of-type").textContent = `${e.size} SQ FEET`;
+
         document.querySelector("main").appendChild(clone);
       });
   });

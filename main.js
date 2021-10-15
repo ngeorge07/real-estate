a1908156b4cf526dd9a42b3443aa8924c38b33da
George <geor2335@stud.kea.dk>
2021-10-15 12:48:55 +0200

    grabbed ima the proper way

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function getData() {
   fetch(
-    "https://georgendesign.com/Module8/real-estate/wp-json/wp/v2/house?order=asc"
+    "https://georgendesign.com/Module8/real-estate/wp-json/wp/v2/house?_embed"
   )
     .then((response) => response.json())
     .then(showPosts);
@@ -10,25 +10,21 @@ function showPosts(e) {
   console.log(e);
 
   e.forEach((e) => {
-    fetch(
-      `https://georgendesign.com/Module8/real-estate/wp-json/wp/v2/media/${e.featured_media}`
-    )
-      .then((response) => response.json())
-      .then((i) => {
-        const temp = document.querySelector("template").content;
-        const clone = temp.cloneNode(true);
+    const temp = document.querySelector("template").content;
+    const clone = temp.cloneNode(true);
 
-        clone.querySelector("h2").textContent = e.title.rendered;
+    clone.querySelector("h2").textContent = e.title.rendered;
 
-        clone.querySelector("img").src = i.source_url;
-        clone.querySelector("img").alt = i.alt_text;
+    clone.querySelector("img").src =
+      e._embedded["wp:featuredmedia"][0].source_url;
+    clone.querySelector("img").alt =
+      e._embedded["wp:featuredmedia"][0].alt_text;
 
-        clone.querySelector("p:first-of-type").textContent = `${e.bed} BED`;
-        clone.querySelector("p:nth-of-type(2)").textContent = `${e.bath} BATH`;
-        clone.querySelector("p:last-of-type").textContent = `${e.size} SQ FEET`;
+    clone.querySelector("p:first-of-type").textContent = `${e.bed} BED`;
+    clone.querySelector("p:nth-of-type(2)").textContent = `${e.bath} BATH`;
+    clone.querySelector("p:last-of-type").textContent = `${e.size} SQ FEET`;
 
-        document.querySelector("main").appendChild(clone);
-      });
+    document.querySelector("main").appendChild(clone);
   });
 }
 
